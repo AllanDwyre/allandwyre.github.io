b6da8641aa1861794834e577d60f2862524e95a2
AllanDwyre <allan.dwyre@gmail.com>
2026-09-11 10:15:40 +0200

    ✨ Animate button icon as a color-cycling reel on hover
    On hover, the icon scrolls through a vertical reel of 4 brand-colored
    copies (red, orange, light orange, yellow) before settling back on the
    normal icon, with a slight scale pop for punch.

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -4,13 +4,22 @@
 
 <button {onclick} class:secondary>
 	{#if Icon}
-		<Icon />
+		<span class="icon-wrap">
+			<span class="icon-reel">
+				<span class="tint"><Icon /></span>
+				<span class="tint tint-yellow"><Icon /></span>
+				<span class="tint tint-light-orange"><Icon /></span>
+				<span class="tint tint-orange"><Icon /></span>
+				<span class="tint tint-red"><Icon /></span>
+				<span class="tint"><Icon /></span>
+			</span>
+		</span>
 	{/if}
 	<span>{content}</span>
 </button>
 
 <style lang="scss">
-	@use '../../styles/_variables.scss' as *;
+	@use '../../styles/variables' as *;
 
 	button {
 		display: flex;
@@ -42,5 +51,81 @@
 		color: $primary;
 
 		border: 2px solid $primary;
+	}
+
+	$icon-size: 24px;
+
+	.icon-wrap {
+		display: inline-flex;
+		flex: none;
+		width: $icon-size;
+		height: $icon-size;
+		overflow: hidden;
+	}
+
+	.icon-reel {
+		display: flex;
+		flex-direction: column;
+		// Repos = dernier cran de la pile (l'icone normale du bas), aligne
+		// avec ce que montre l'animation une fois revenue a sa base.
+		transform: translateY(calc($icon-size * -5));
+	}
+
+	.tint {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex: none;
+		width: $icon-size;
+		height: $icon-size;
+		color: inherit;
+	}
+
+	.tint-red {
+		color: $color-red;
+	}
+
+	.tint-orange {
+		color: $color-orange;
+	}
+
+	.tint-light-orange {
+		color: $color-light-orange;
+	}
+
+	.tint-yellow {
+		color: $color-yellow;
+	}
+
+	button:hover .icon-reel {
+		animation: icon-reel 800ms cubic-bezier(0.65, 0, 0.35, 1);
+	}
+
+	button:hover .icon-wrap {
+		animation: icon-pop 800ms cubic-bezier(0.33, 1, 0.68, 1);
+	}
+
+	@keyframes icon-reel {
+		from {
+			// Meme valeur que le repos : le reel demarre "en haut" de sa pile
+			// (icone normale) et descend, teinte par teinte, jusqu'a l'icone
+			// normale du haut - defilement inverse du premier essai.
+			transform: translateY(calc($icon-size * -5));
+		}
+		to {
+			transform: translateY(0);
+		}
+	}
+
+	@keyframes icon-pop {
+		0% {
+			transform: scale(1);
+		}
+		45% {
+			transform: scale(1.15);
+		}
+		100% {
+			transform: scale(1);
+		}
 	}
 </style>
